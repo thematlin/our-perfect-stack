d4d800aba66f8c728b83821636ec8feff6e652e9
Mattias Lindahl <mattias@mlindahl.nu>
2014-06-13 11:08:53 +0200

    durandal task working nicely

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,24 @@ var gulp = require('gulp'),
     durandal = require('gulp-durandal'),
     watch = require('gulp-watch'),
     connect = require('gulp-connect'),
-    less = require('gulp-less-sourcemap');
+    less = require('gulp-less-sourcemap'),
+    clean = require('gulp-clean');
 
-gulp.src(['src/**/*', 'bower_components/requirejs-text/text.js'])
-  .pipe(gulp.dest('.tmp/src'));
+
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
-gulp.task('durandal', function() {
+gulp.task('copy-to-tmp', function() {
+  return gulp.src(['src/**/*', 'bower_components/requirejs-text/text.js'])
+  .pipe(gulp.dest('.tmp/src'));
+})
+
+gulp.task('durandal', ['copy-to-tmp'], function() {
   return durandal({
     baseDir: '.tmp/src',
     minify: true,
-  }).pipe(gulp.dest('dist/'));
+  }).pipe(gulp.dest('dist/'))
+    .pipe(gulp.src('.tmp').pipe(clean()));
 });
