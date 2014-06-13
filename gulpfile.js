6d77d5f99a20e24e7b015dff5b220901c74e7bb8
Mattias Lindahl <mattias@mlindahl.nu>
2014-06-13 10:19:22 +0200

    added tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    durandal = require('gulp-durandal'),
+    watch = require('gulp-watch'),
+    connect = require('gulp-connect'),
+    less = require('gulp-less-sourcemap');
 
 gulp.task('default', function() {
   // place code for your default task here
