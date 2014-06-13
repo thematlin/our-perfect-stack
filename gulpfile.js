66b896437951d41e0331a60bd7f9af2132779e4a
Mattias Lindahl <mattias@mlindahl.nu>
2014-06-13 12:55:43 +0200

    watch and connect modules are now working

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,42 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     connect = require('gulp-connect'),
     less = require('gulp-less-sourcemap'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    jshint = require('gulp-jshint'),
+    debug = require('gulp-debug');
 
+gulp.task('default', ['connect', 'durandal', 'watch']);
 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'dist/src',
+    livereload: true,
+    port: 1339
+  });
+});
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('assets', ['durandal'], function() {
+  return gulp.src(['src/assets/**/*.js', 'src/assets/**/*.html'])
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  watch({glob: ['src/**/*.js', 'src/**/*.html'], verbose: true}, ['assets']);
+});
+
+gulp.task('jshint', function() {
+  return gulp.src('src/assets/**/*.js')
 });
 
 gulp.task('copy-to-tmp', function() {
   return gulp.src(['src/**/*', 'bower_components/requirejs-text/text.js'])
   .pipe(gulp.dest('.tmp/src'));
-})
+});
 
 gulp.task('durandal', ['copy-to-tmp'], function() {
   return durandal({
     baseDir: '.tmp/src',
     minify: true,
   }).pipe(gulp.dest('dist/'))
-    .pipe(gulp.src('.tmp').pipe(clean()));
+    .pipe(gulp.src('.tmp').pipe(clean()))
 });
