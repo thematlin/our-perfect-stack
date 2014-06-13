e0c532d83d42ee70547572be04bd80a55b46bb71
Mattias Lindahl <mattias@mlindahl.nu>
2014-06-13 10:55:56 +0200

    added copy and durandal task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,16 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     less = require('gulp-less-sourcemap');
 
+gulp.src(['src/**/*', 'bower_components/requirejs-text/text.js'])
+  .pipe(gulp.dest('.tmp/src'));
+
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('durandal', function() {
+  return durandal({
+    baseDir: '.tmp/src',
+    minify: true,
+  }).pipe(gulp.dest('dist/'));
 });
